d77a6b97a827421993c4491962190d9da93f5726
Justin Blake <justin@hentzia.com>
2011-05-17 17:37:42 -0400

    Use VCR to fake crowd api interaction in specs.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,11 +2,32 @@ require "rubygems"
 require "bundler/setup"
 
 require "rspec"
-RSpec.configure do |config|
-  config.fail_fast = true
-end
+require "vcr"
 
 require "crowd_rest"
+
 CrowdRest.crowd_url = "http://127.0.0.1:8095"
 CrowdRest.app_name = "demo"
 CrowdRest.app_pass = "demo_pass"
+
+VCR.config do |c|
+  c.cassette_library_dir = File.expand_path("cassette_library", File.dirname(__FILE__))
+  c.stub_with :fakeweb
+  c.ignore_localhost = false
+end
+
+if ENV['DONT_FAKE']
+  puts "VCR: Will record all http requests made during this test run..."
+  record_type = :all
+  at_exit { VCR.eject_cassette }
+else
+  puts "VCR: Using previously recorded http requests. " +
+       "Run with DONT_FAKE=1 to hit the web and record new requests."
+  record_type = :none
+end
+
+VCR.insert_cassette('test_data', :record => record_type)
+
+RSpec.configure do |config|
+  config.extend VCR::RSpec::Macros
+end
